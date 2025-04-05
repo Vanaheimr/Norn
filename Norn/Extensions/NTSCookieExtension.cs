fb7045a7e3e1eb02b42f6dbce20b384374f21849
Achim Friedland <achim.friedland@graphdefined.com>
2025-04-05 20:22:32 +0200

    Imported code from Hermod

---
+++ Norn/Extensions/NTSCookieExtension.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright (c) 2010-2025 GraphDefined GmbH <achim.friedland@graphdefined.com>
+ * This file is part of Vanaheimr Norn <https://www.github.com/Vanaheimr/Norn>
+ *
+ * Licensed under the Affero GPL license, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System.Security.Cryptography;
+using System.Diagnostics.CodeAnalysis;
+
+using org.GraphDefined.Vanaheimr.Illias;
+
+#endregion
+
+namespace org.GraphDefined.Vanaheimr.Norn.NTP
+{
+
+    public class NTSCookieExtension(Byte[]   Cookie,
+                                    Boolean  Authenticated   = false,
+                                    Boolean  Encrypted       = false) : NTPExtension(ExtensionTypes.NTSCookie,
+                                                                                     Cookie,
+                                                                                     Authenticated,
+                                                                                     Encrypted)
+    {
+
+
+
+    }
+
+}
